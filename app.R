6d08377d248c2eeabdabd667db1571a5ca611c90
slopp <sean@rstudio.com>
2019-06-13 20:33:37 +0000

    auto change detection

--- app.R
+++ app.R
@@ -1,7 +1,7 @@
 library(shiny)
 
 ui <- shiny::sidebarLayout(
-  titlePanel("I'm alive! And different!"),
+  titlePanel("I'm alive! And different! Will it pick up the change?"),
   mainPanel()
 )
 server <- function(input, output, session){
