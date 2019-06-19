97453d10f245c039d58be18fc831e1049b2f530a
Sean Lopp <sean@rstudio.com>
2019-06-19 09:39:31 -0600

    Update app.R

--- app.R
+++ app.R
@@ -1,7 +1,7 @@
 library(shiny)
 
 ui <- shiny::sidebarLayout(
-  titlePanel("I'm alive! A new commit!"),
+  titlePanel("I'm alive! Hello CS!"),
   mainPanel()
 )
 server <- function(input, output, session){
